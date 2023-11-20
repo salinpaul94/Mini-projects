13d0c8cb34ceac26df915c31a135a1d83a056b9c
salinpaul valooran <salinpv@hotmail.com>
2023-11-20 20:48:31 +0530

    const for mappedIndex

--- 003_font_breathing/index.js
+++ 003_font_breathing/index.js
@@ -1,4 +1,6 @@
 const spans = document.querySelectorAll("p span");
 const numLetters = spans.length;
 
-spans.forEach(function (span, i) {});
+spans.forEach(function (span, i) {
+  const mappedIndex = i - (numLetters / 2)
+});
