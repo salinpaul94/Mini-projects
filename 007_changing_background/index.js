bfd8649cf6ab5aba1aad6170369031e2d8bf9925
salinpaul valooran <salinpv@hotmail.com>
2023-11-22 18:57:52 +0530

    setting value colorIndices[3]

--- 007_changing_background/index.js
+++ 007_changing_background/index.js
@@ -47,5 +47,8 @@ function updateGradient() {
     colorIndices[1] =
       (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
       colors.length;
+    colorIndices[3] =
+      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
   }
 }
