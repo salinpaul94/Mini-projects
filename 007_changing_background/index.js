fb35956305c9e0f154aead5c065cb1632428f7b9
salinpaul valooran <salinpv@hotmail.com>
2023-11-22 18:49:58 +0530

    variable for color 2

--- 007_changing_background/index.js
+++ 007_changing_background/index.js
@@ -27,5 +27,6 @@ function updateGradient() {
 
   let r2 = Math.round(istep * c1_0[0] + step * c1_1[1]);
   let g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  let b1 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  let b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  let color2 = `rgb(${r2},${g2},${b2})`;
 }
